7ba95ff0459f11b0e40f6978151f5450000aa24d
M.M.Eslami <130648329+MMEslami123@users.noreply.github.com>
2024-08-13 15:59:55 +0330

    define a timer and manage analoges

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const second = document.querySelector(".second-rotate");
 const minute = document.querySelector(".minute-rotate");
 const hour = document.querySelector(".hour-rotate");
+setInterval(timeHandler, 1000);
+function timeHandler() {
+  let getTime = new Date();
+  let myHour = getTime.getHours() * 30;
+  let myMin = getTime.getMinutes() * 6;
+  let mySec = getTime.getSeconds() * 6;
+}
